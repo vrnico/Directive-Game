d7a9ccbfe87a7a2a871363a6a47b455812318f47
vrnico <nico.daunt@gmail.com>
2018-06-15 12:55:07 -0700

    dynamically set highlight values from outside

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -3,28 +3,31 @@ import { Directive,
   ElementRef,
   Renderer2,
   HostListener,
-  HostBinding } from '@angular/core';
+  HostBinding,
+Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'red';
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef:  ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
     @HostListener('mouseenter') mouseover(eventData: Event){
           // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-          this.backgroundColor = 'blue';
+          this.backgroundColor = this.highlightColor;
     }
 
     @HostListener('mouseleave') mouseleave(eventData: Event){
           // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-            this.backgroundColor = 'yellow';
+            this.backgroundColor = this.defaultColor;
     }
 }
